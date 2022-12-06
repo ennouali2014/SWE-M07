227e68d17e549685efb3a098e2725282cbda75bb
dabourAkademie <mdabour@hotmail.com>
2022-12-06 12:20:23 +0100

    Klasse Person bearbeitet

--- src/main/java/de/unibremen/akademie/kursverwaltung/module/Person.java
+++ src/main/java/de/unibremen/akademie/kursverwaltung/module/Person.java
@@ -1,15 +1,16 @@
 package de.unibremen.akademie.kursverwaltung.module;
 
-import java.util.Date;
-
 public class Person {
 
+    private Anrede anrede;
+    private String titel;
     private String name;
     private String vorname;
-    private Date geburtsdatum;
     private String strasse;
     private String plz;
     private String ort;
+    private String email;
+    private String telNr;
 
 
 }
