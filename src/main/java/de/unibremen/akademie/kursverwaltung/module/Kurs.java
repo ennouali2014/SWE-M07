162385f0e991d3af348064544c94e0b7b58a8334
Yolla <yhanna@uni-bremen.de>
2022-12-06 12:30:32 +0100

    kurs klasse Attribute

---
+++ src/main/java/de/unibremen/akademie/kursverwaltung/module/Kurs.java
@@ -0,0 +1,23 @@
+package de.unibremen.akademie.kursverwaltung.module;
+
+import java.util.Date;
+
+public class Kurs {
+
+    private String name;
+    private int stunde;
+    private int zyklus;
+    private Date startDatum;
+    private Date endeDatum;
+    private int aktuelleTnZahl;
+    private int minTnZahl;
+    private int maxTnZahl;
+    private int freiePlaetze;
+    private double gebuehrBrutto;
+    private double gebuehrNetto;
+    private double mwstEuro;
+    private double mwstProzent;
+    private String kursBeschreibung;
+
+
+}
