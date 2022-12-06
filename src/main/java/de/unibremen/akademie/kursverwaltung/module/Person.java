51e311848eaa172d02523e974b97fc22c745ba9a
dabourAkademie <mdabour@hotmail.com>
2022-12-06 12:04:06 +0100

    datei modul angelegt

---
+++ src/main/java/de/unibremen/akademie/kursverwaltung/module/Person.java
@@ -0,0 +1,15 @@
+package de.unibremen.akademie.kursverwaltung.module;
+
+import java.util.Date;
+
+public class Person {
+
+    private String name;
+    private String vorname;
+    private Date geburtsdatum;
+    private String strasse;
+    private String plz;
+    private String ort;
+
+
+}
