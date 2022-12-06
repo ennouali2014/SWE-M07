d196a07f3dd0e335f8279c43acf12189cf7a2a46
Yolla <yhanna@uni-bremen.de>
2022-12-06 13:33:45 +0100

    Konstruktor setter Getter

--- src/main/java/de/unibremen/akademie/kursverwaltung/domain/Kurs.java
+++ src/main/java/de/unibremen/akademie/kursverwaltung/domain/Kurs.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class Kurs {
 
     private String name;
-    private int stunde;
+    private int anzahlTage;
     private int zyklus;
     private Date startDatum;
     private Date endeDatum;
@@ -19,5 +19,127 @@ public class Kurs {
     private double mwstProzent;
     private String kursBeschreibung;
 
+    public Kurs(String name, int anzahlTage, int zyklus, Date startDatum, int minTnZahl, int maxTnZahl, double gebuehrBrutto, double mwstProzent, String kursBeschreibung) {
+        this.name = name;
+        this.anzahlTage = anzahlTage;
+        this.zyklus = zyklus;
+        this.startDatum = startDatum;
+        this.minTnZahl = minTnZahl;
+        this.maxTnZahl = maxTnZahl;
+        this.gebuehrBrutto = gebuehrBrutto;
+        this.mwstProzent = mwstProzent;
+        this.kursBeschreibung = kursBeschreibung;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAnzahlTage() {
+        return anzahlTage;
+    }
+
+    public void setAnzahlTage(int anzahlTage) {
+        this.anzahlTage = anzahlTage;
+    }
+
+    public int getZyklus() {
+        return zyklus;
+    }
+
+    public void setZyklus(int zyklus) {
+        this.zyklus = zyklus;
+    }
+
+    public Date getStartDatum() {
+        return startDatum;
+    }
+
+    public void setStartDatum(Date startDatum) {
+        this.startDatum = startDatum;
+    }
+
+    public Date getEndeDatum() {
+        return endeDatum;
+    }
+
+    public void setEndeDatum(Date endeDatum) {
+        this.endeDatum = endeDatum;
+    }
+
+    public int getAktuelleTnZahl() {
+        return aktuelleTnZahl;
+    }
+
+    public void setAktuelleTnZahl(int aktuelleTnZahl) {
+        this.aktuelleTnZahl = aktuelleTnZahl;
+    }
+
+    public int getMinTnZahl() {
+        return minTnZahl;
+    }
+
+    public void setMinTnZahl(int minTnZahl) {
+        this.minTnZahl = minTnZahl;
+    }
+
+    public int getMaxTnZahl() {
+        return maxTnZahl;
+    }
+
+    public void setMaxTnZahl(int maxTnZahl) {
+        this.maxTnZahl = maxTnZahl;
+    }
+
+    public int getFreiePlaetze() {
+        return freiePlaetze;
+    }
+
+    public void setFreiePlaetze(int freiePlaetze) {
+        this.freiePlaetze = freiePlaetze;
+    }
+
+    public double getGebuehrBrutto() {
+        return gebuehrBrutto;
+    }
+
+    public void setGebuehrBrutto(double gebuehrBrutto) {
+        this.gebuehrBrutto = gebuehrBrutto;
+    }
+
+    public double getGebuehrNetto() {
+        return gebuehrNetto;
+    }
+
+    public void setGebuehrNetto(double gebuehrNetto) {
+        this.gebuehrNetto = gebuehrNetto;
+    }
+
+    public double getMwstEuro() {
+        return mwstEuro;
+    }
+
+    public void setMwstEuro(double mwstEuro) {
+        this.mwstEuro = mwstEuro;
+    }
+
+    public double getMwstProzent() {
+        return mwstProzent;
+    }
+
+    public void setMwstProzent(double mwstProzent) {
+        this.mwstProzent = mwstProzent;
+    }
+
+    public String getKursBeschreibung() {
+        return kursBeschreibung;
+    }
+
+    public void setKursBeschreibung(String kursBeschreibung) {
+        this.kursBeschreibung = kursBeschreibung;
+    }
 }
