c4490dd27c6cfad51c7a3ca7e89a6a942625ff7c
th62 <haberland@webtaktik.de>
2022-12-07 13:31:54 +0100

    emailtest vereinfacht

--- src/Test/de/unibremen/akademie/kursverwaltung/domain/PersonTest.java
+++ src/Test/de/unibremen/akademie/kursverwaltung/domain/PersonTest.java
@@ -2,20 +2,22 @@ package de.unibremen.akademie.kursverwaltung.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class PersonTest {
 
     @Test
     void testCheckValidEmail() {
-        assertEquals(true, Person.checkValidEmail("mdabour@hotmail.de"));
-        assertEquals(false, Person.checkValidEmail("mdabour@hotmail..international"));
-        assertEquals(true, Person.checkValidEmail("mdabour@hotmail.mail.international"));
-        assertEquals(false, Person.checkValidEmail("@hotmail"));
-        assertEquals(false, Person.checkValidEmail("@hotmail.de"));
-        assertEquals(false, Person.checkValidEmail("@hotmail.d"));
-        assertEquals(false, Person.checkValidEmail("m@.de"));
-        assertEquals(true, Person.checkValidEmail("m@d.de"));
-        assertEquals(false, Person.checkValidEmail("m@d..de"));
+        assertTrue(Person.checkValidEmail("mdabour@hotmail.de"));
+        assertFalse(Person.checkValidEmail("mdabour@hotmail..international"));
+        assertTrue(Person.checkValidEmail("mdabour@hotmail.mail.international"));
+        assertFalse(Person.checkValidEmail("@hotmail"));
+        assertFalse(Person.checkValidEmail("@hotmail.de"));
+        assertFalse(Person.checkValidEmail("@hotmail.d"));
+        assertFalse(Person.checkValidEmail("m@.de"));
+        assertTrue(Person.checkValidEmail("m@d.de"));
+        assertFalse(Person.checkValidEmail("m@d..de"));
 
 
     }
