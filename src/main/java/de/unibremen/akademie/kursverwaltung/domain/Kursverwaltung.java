8420e7dc3c703d4927e528f1b2df5de8661aaef3
fennnouali <ennouali@uni-bremen.de>
2022-12-08 11:28:14 +0100

    kursverwaltung

--- src/main/java/de/unibremen/akademie/kursverwaltung/domain/Kursverwaltung.java
+++ src/main/java/de/unibremen/akademie/kursverwaltung/domain/Kursverwaltung.java
@@ -9,29 +9,22 @@ public class Kursverwaltung {
     private List<Kurs> kursList=new ArrayList<>();
 
     public String addnewKurs( String name, int anzahlTage, int zyklus, Date startDatum, int maxTnZahl, int minTnZahl,
-                                    double gebuehrBrutto, double mwstProzent, String kursBeschreibung, Person person, Person person1) {
+                                    double gebuehrBrutto, double mwstProzent, String kursBeschreibung) {
         Kurs kurs = new Kurs();
         kurs.setKursBeschreibung(kursBeschreibung);
-        if(kurs.setName(name)==false){
-
-        };
-        kurs.setAnzahlTage(anzahlTage);
-        kurs.setZyklus(zyklus);
-        kurs.setStartDatum(startDatum);
-        if (kurs.setMaxTnZahl(maxTnZahl) == false ){
-            return "";
-        };
+        if(kurs.setName(name)==false){return "Name is Require";}
+        if(kurs.setAnzahlTage(anzahlTage)==false){return "minimum teilnahme ist falsch";}
+        if(kurs.setZyklus(zyklus)==false){return "zyklus is Require";}
+        if(kurs.setStartDatum(startDatum)==false){return " Start Date is Require";}
+        if(kurs.setMaxTnZahl(maxTnZahl)==false){return " Max anzahl darf nicht weniger als Min anzahl der Teilnehmer";};
         if(kurs.setMinTnZahl(minTnZahl)==false){return "minimum teilnahme ist falsch";}
         if(kurs.setGebuehrBrutto(gebuehrBrutto)==false){return "gebuhr Brutto ist falssch";}
-        kurs.setMwstProzent(mwstProzent);
-        kurs.setInteressentenListe(person);
+        if(kurs.setMwstProzent(mwstProzent)==false){return "prozent MWST is Require";}
         kurs.setEndeDatum(startDatum, zyklus, anzahlTage);
         kurs.setGebuehrNetto(gebuehrBrutto, mwstProzent);
         kurs.setMwstEuro(mwstProzent, gebuehrBrutto);
-        kurs.setTeilnehmerListe(person);
-        kurs.setInteressentenListe(person1);
         kurs.setAktuelleTnZahl();
         kursList.add(kurs);
-        return null;
+        return "OK";
     }
 }
