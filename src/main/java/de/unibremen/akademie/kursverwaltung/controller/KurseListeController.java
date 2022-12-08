0ac6ebf2a3891c04c50630f88bee7eeb22bcb6c3
Akademie <othiel@uni-bremen.de>
2022-12-08 11:50:45 +0100

    kurslisteController

--- src/main/java/de/unibremen/akademie/kursverwaltung/controller/KurseListeController.java
+++ src/main/java/de/unibremen/akademie/kursverwaltung/controller/KurseListeController.java
@@ -1,4 +1,77 @@
+
 package de.unibremen.akademie.kursverwaltung.controller;
 
+        import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.DatePicker;
+        import javafx.scene.control.Label;
+        import javafx.scene.control.MenuButton;
+        import javafx.scene.control.TextField;
+
 public class KurseListeController {
+
+    @FXML
+    private DatePicker abDatumDatePicker;
+
+    @FXML
+    private MenuButton alleMenuButton;
+
+    @FXML
+    private Button bearbeitenButton;
+
+    @FXML
+    private DatePicker bisDatumDatePicker;
+
+    @FXML
+    private DatePicker datePicker1;
+
+    @FXML
+    private Button entfernenButton;
+
+    @FXML
+    private Button hinzufugenButton;
+
+    @FXML
+    private TextField kursNameTextField;
+
+    @FXML
+    private Label lblAbDatum;
+
+    @FXML
+    private Label lblBisDatum;
+
+    @FXML
+    private Label lblTextField;
+
+    @FXML
+    void abDatselectDate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bearbeitenButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bisDatSelectDate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void entfernenButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hinzufugenButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void s(ActionEvent event) {
+
+    }
+
 }
