643e3d35d34d3e4f273ba1f267ddccf6701f9176
Tithin <ttomy@uni-bremen.de>
2023-01-02 11:53:44 +0100

    initialize method hinzufügt

--- src/main/java/de/unibremen/akademie/kursverwaltung/controller/PersonenDetailsController.java
+++ src/main/java/de/unibremen/akademie/kursverwaltung/controller/PersonenDetailsController.java
@@ -1,13 +1,15 @@
 package de.unibremen.akademie.kursverwaltung.controller;
 
 import de.unibremen.akademie.kursverwaltung.domain.Kursverwaltung;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
 public class PersonenDetailsController {
     @FXML
-    public TextField anrede;
+    //public TextField anrede;
     public TextField titel;
     public TextField name;
     public TextField vorname;
@@ -16,12 +18,19 @@ public class PersonenDetailsController {
     public TextField email;
     public TextField telefon;
     public TextField ort;
+    public ChoiceBox Anrede;
     public Button save;
     public String nameS;
 
+    public void initialize() {
+        Anrede.setItems(FXCollections.observableArrayList(de.unibremen.akademie.kursverwaltung.domain.Anrede.values()));
+
+    }
+
     @FXML
     public void onsaveclick() {
-        Kursverwaltung.addPerson(name.getText(), vorname.getText(), strasse.getText(), plz.getText(), ort.getText(), email.getText(),telefon.getText());
+
+        Kursverwaltung.addPerson(name.getText(), vorname.getText(), strasse.getText(), plz.getText(), ort.getText(), email.getText(), telefon.getText());
         name.clear();
         vorname.clear();
         strasse.clear();
