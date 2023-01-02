5bcac171189a86b777ab8c2a23c1fe8d4ba84aa2
Alex <afoerster@me.com>
2023-01-02 09:21:29 +0100

    laden speichern Idee

--- src/main/java/de/unibremen/akademie/kursverwaltung/MainApplication.java
+++ src/main/java/de/unibremen/akademie/kursverwaltung/MainApplication.java
@@ -12,9 +12,11 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
+        // Daten laden
         stage.setTitle("Kursverwaltung v 1.0");
         stage.setScene(scene);
         stage.show();
+        // Daten speichern
     }
 
     public static void main(String[] args) {
