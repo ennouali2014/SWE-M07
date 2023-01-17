c43ef6cf5e0b7b11d9c45ffeea514a2933114c74
fennnouali <ennouali@uni-bremen.de>
2023-01-17 11:57:19 +0100

    update/anzeigen.

--- src/main/java/de/unibremen/akademie/kursverwaltung/controller/KurseListeController.java
+++ src/main/java/de/unibremen/akademie/kursverwaltung/controller/KurseListeController.java
@@ -143,12 +143,14 @@ public class KurseListeController {
 
     @FXML
     void hinzufugenButtonAction(ActionEvent event) {
-        KvModel.aktuelleKurs = null;
-        main.fxmlKurseDetailsController.abbrechen(event);
-        for (Tab tabPaneKursAnlegen : fxmlKurseListe.getTabPane().getTabs()) {
-            if (tabPaneKursAnlegen.getText().equals("Kurse-Details")) {
-                tabPaneKursAnlegen.getTabPane().getSelectionModel().select(tabPaneKursAnlegen);
+        if (!tableView.getSelectionModel().isEmpty() && tableView.getSelectionModel().getSelectedItems().size() < 2) {
+            KvModel.aktuelleKurs = null;
+            main.fxmlKurseDetailsController.abbrechen(event);
+            for (Tab tabPaneKursAnlegen : fxmlKurseListe.getTabPane().getTabs()) {
+                if (tabPaneKursAnlegen.getText().equals("Kurse-Details")) {
+                    tabPaneKursAnlegen.getTabPane().getSelectionModel().select(tabPaneKursAnlegen);
 
+                }
             }
         }
     }
