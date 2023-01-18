9c60a2bacf813e8e3a4c3f243cdf3da7d6ce69aa
cody <mpawel@uni-bremen.de>
2023-01-18 20:53:26 +0100

    person nach update in liste noch selektiert, auch nach umsortierung der liste

--- src/main/java/de/unibremen/akademie/kursverwaltung/controller/PersonenDetailsController.java
+++ src/main/java/de/unibremen/akademie/kursverwaltung/controller/PersonenDetailsController.java
@@ -9,11 +9,10 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 
-import java.util.List;
-
 public class PersonenDetailsController {
     @FXML
     public ChoiceBox anrede;
+
     @FXML
     public TextField titel;
     @FXML
@@ -32,15 +31,15 @@ public class PersonenDetailsController {
     public TextField ort;
     @FXML
     public Button save;
+
     @FXML
     public Button abbrechen;
-    @FXML
+
     public ObservableList<String> choiceListAnrede = FXCollections.observableArrayList();
     static public final ObservableList<Person> listKursTeilnehmer =
             FXCollections.observableArrayList();
     static public final ObservableList<Person> listKursInteressent =
             FXCollections.observableArrayList();
-    @FXML
     public Tab fxmlPersonenDetails;
 
     static public boolean zurueckPersonenliste = false;
@@ -96,9 +95,11 @@ public class PersonenDetailsController {
         startDate.setCellValueFactory(new PropertyValueFactory<Kurs, String>("displaystartDate"));
         startDate.setCellFactory(TextFieldTableCell.<Kurs>forTableColumn());
 
+
         tableViewKurse.setItems(KvModel.model.kursList);
         TableView.TableViewSelectionModel<Kurs> selectionModel =
                 tableViewKurse.getSelectionModel();
+
 
         tableViewKurse.getSelectionModel();
         selectionModel.setSelectionMode(SelectionMode.SINGLE);
@@ -107,17 +108,18 @@ public class PersonenDetailsController {
         kursZuTeilnehmer.setCellFactory(TextFieldTableCell.<Kurs>forTableColumn());
 //        tableViewKurse.getSelectionModel().selectedItemProperty().addListener(
 //                (observable, oldValue, newValue) -> System.out.println(newValue));
+
+
     }
 
     @FXML
-    public void onClickSavePerson() {
+    public void onsaveclick() {
         Person person = null;
         // Update einer bestehenden Person
         if (KvModel.aktuellePerson != null) {
             try {
                 KvModel.aktuellePerson.updatePerson(anrede.getValue().toString(), titel.getText(), vorname.getText(), nachname.getText(), strasse.getText(), plz.getText(), ort.getText(), email.getText(), telefon.getText());
                 person = KvModel.aktuellePerson;
-                KvModel.personKursList.addAll(PersonKursListe.personKursList);
             } catch (Exception e) {
                 Meldung.eingabeFehler(e.getMessage());
                 return;
@@ -147,12 +149,13 @@ public class PersonenDetailsController {
 
             main.fxmlPersonenListeController.table.getSelectionModel().clearSelection();
             main.fxmlPersonenListeController.table.getSelectionModel().select(person);
+
         }
     }
 
   
     @FXML
-    public void onClickAnzeigeAendernPerson(Person person) {
+    public void anzeigeZumAendern(Person person) {
         if (person != null) {
             String anrede = person.getAnrede();
             for (int i = 0; i < choiceListAnrede.size(); i++) {
@@ -172,7 +175,7 @@ public class PersonenDetailsController {
     }
 
     @FXML
-    public void onClickAbbrechenPerson(ActionEvent event) {
+    public void onabbrechenclick(ActionEvent event) {
         felderLeeren();
 
         if (zurueckPersonenliste) {
@@ -207,6 +210,8 @@ public class PersonenDetailsController {
     }
 
     public void ausTeilnehmerRaus(ActionEvent actionEvent) {
+
+
     }
 
     /*
@@ -231,14 +236,19 @@ public class PersonenDetailsController {
         if (KvModel.aktuellePerson == null || tableViewKurse.getSelectionModel().getSelectedItem() == null) {
             return;
         }
-        Boolean test_is_kurs = PersonKursListe.modelKP.addPersonInKursAlsTeilnehmer(KvModel.aktuellePerson,
+        Boolean test_add_kurs = PersonKursListe.modelKP.addPersonInKursAlsTeilnehmer(KvModel.aktuellePerson,
                 (Kurs) tableViewKurse.getSelectionModel().getSelectedItem());
-
-        if (test_is_kurs) {
+//    Kurs kurs = (Kurs) tableViewKurse.getSelectionModel().getSelectedItem();
+//    KvModel.aktuellePerson.addKursTeilnehmer(kurs); //
+        if (test_add_kurs) {
             tableViewTeilnehmerZu.getItems().add(tableViewKurse.getSelectionModel().getSelectedItem());
         }
 
+//    System.out.println(KvModel.aktuellePerson);
+//    System.out.println(tableViewKurse.getSelectionModel().getSelectedItem());
+
     }
+
 
     public void kursZuInteressent(ActionEvent actionEvent) {
     }
