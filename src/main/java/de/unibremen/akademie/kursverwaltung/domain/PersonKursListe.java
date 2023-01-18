b8778011b37ac3413ffc2302462490cab9d0d461
th62 <haberland@webtaktik.de>
2023-01-18 09:35:26 +0100

    Load/Save PersonKursListe - bugfixing

--- src/main/java/de/unibremen/akademie/kursverwaltung/domain/PersonKursListe.java
+++ src/main/java/de/unibremen/akademie/kursverwaltung/domain/PersonKursListe.java
@@ -13,17 +13,17 @@ public class PersonKursListe {
     static public final ObservableList<PersonKurs> personKursList =
             FXCollections.observableArrayList();
 //Mohamed
-    public Boolean addPersonInKursAlsTeilnehmer(Person person, Kurs kurs) {
+public static Boolean addPersonInKursAlsTeilnehmer(Person person, Kurs kurs) {
 
-        for (PersonKurs personKurs : this.personKursList) {
-            if (personKurs.getPerson().equals(person) && personKurs.getKurs().equals(kurs)) {
-                System.out.println("Ich bin hier1" + personKursList.size());
-                return false;
-            }
+    for (PersonKurs personKurs : personKursList) {
+        if (personKurs.getPerson().equals(person) && personKurs.getKurs().equals(kurs)) {
+            System.out.println("Ich bin hier1" + personKursList.size());
+            return false;
         }
-        PersonKurs personKurs = new PersonKurs(person, kurs, true);
-        System.out.println("Ich bin hier2" + personKursList.size());
-        this.personKursList.add(personKurs);
+    }
+    PersonKurs personKurs = new PersonKurs(person, kurs, true);
+    System.out.println("Ich bin hier2" + personKursList.size());
+    personKursList.add(personKurs);
         return true;
     }
 
