5a92edc357c6225ff06ee7926034882f36848ae9
Alex <afoerster@me.com>
2023-01-19 16:52:43 +0100

    Einiges in PersonKusrListe gefixt und im PersonenDetailsController

--- src/main/java/de/unibremen/akademie/kursverwaltung/domain/PersonKursListe.java
+++ src/main/java/de/unibremen/akademie/kursverwaltung/domain/PersonKursListe.java
@@ -13,36 +13,66 @@ public class PersonKursListe {
             FXCollections.observableArrayList();
 
 
-    public Boolean addPersonInKursAlsTeilnehmer(Person person, Kurs kurs) {
+    // TODO: wird jetzt nicht unbedingt nur geADDed, sondern ggf. geändert. Sollte das besser in einer anderen Methode gemacht werden? (AxF)
+    public Boolean addPersonInKurs(Person person, Kurs kurs, Boolean alsTeilnehmer) {
         for (PersonKurs personKurs : personKursList) {
             if (personKurs.getPerson().equals(person) && personKurs.getKurs().equals(kurs)) {
-                return false;
+                if (personKurs.isTeilnehmer() == alsTeilnehmer) {
+                    return false;
+                } else {
+                    personKurs.setTeilnehmer(alsTeilnehmer);
+                    return true;
+                }
             }
         }
         PersonKurs personKurs = new PersonKurs();
         personKurs.setPerson(person);
         personKurs.setKurs(kurs);
-        personKurs.setTeilnehmer(true);
+        personKurs.setTeilnehmer(alsTeilnehmer);
 
         personKursList.add(personKurs);
         return true;
     }
 
-    public List<String> getKurseAlsTeilnehmer(Person person) {
+    public Boolean addPersonInKursAlsInteressent(Person person, Kurs kurs) {
+        return addPersonInKurs(person, kurs, false);
+    }
+
+    public Boolean addPersonInKursAlsTeilnehmer(Person person, Kurs kurs) {
+        return addPersonInKurs(person, kurs, true);
+    }
+
+    public List<String> getKurse(Person person, boolean alsTeilnehmer) {
         List<String> listkurs = new ArrayList<>();
 
         for (PersonKurs personKurs : personKursList) {
-            if (personKurs.getPerson().equals(person)) {
+            if (personKurs.getPerson().equals(person) && personKurs.isTeilnehmer() == alsTeilnehmer) {
                 listkurs.add(personKurs.getKurs().getName());
             }
         }
         return listkurs;
     }
 
+    public List<String> getKurseAlsTeilnehmer(Person person) {
+        return getKurse(person, true);
+    }
+
+    public List<String> getKurseAlsInteressent(Person person) {
+        return getKurse(person, false);
+    }
+
+
+    public void addKurse(Person person, List<Kurs> liste, boolean alsTeilnehmer) {
+        for (var kurs : liste) {
+            addPersonInKurs(person, kurs, alsTeilnehmer);
+        }
+    }
 
     public void addKurseAlsTeilnehmer(Person person, List<Kurs> teilnnehmerliste) {
-        for (var kurs : teilnnehmerliste) {
-            addPersonInKursAlsTeilnehmer(person, kurs);
-        }
+        addKurse(person, teilnnehmerliste, true);
+    }
+
+    public void addKurseAlsInteressent(Person person, List<Kurs> interessentenliste) {
+        addKurse(person, interessentenliste, false);
     }
 }
