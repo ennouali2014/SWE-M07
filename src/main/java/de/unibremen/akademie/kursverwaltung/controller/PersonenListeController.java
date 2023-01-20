39e6b1006d05eb20582dab06d4537aff9c23ee8a
th62 <haberland@webtaktik.de>
2023-01-20 13:15:52 +0100

    Refractoring nach StylGuide v2.5 (PersonenListeController)

--- src/main/java/de/unibremen/akademie/kursverwaltung/controller/PersonenListeController.java
+++ src/main/java/de/unibremen/akademie/kursverwaltung/controller/PersonenListeController.java
@@ -88,7 +88,7 @@ public class PersonenListeController implements Initializable {
     }
 
     @FXML
-    public void andernButtonAction(ActionEvent event) {
+    public void onClickPersonAendernPersonenListe(ActionEvent event) {
         PersonenDetailsController.zurueckPersonenliste = true;
 
         if (!tablePersonenListe.getSelectionModel().isEmpty()) {
