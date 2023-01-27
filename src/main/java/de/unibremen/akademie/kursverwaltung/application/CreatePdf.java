af5eb3bc912f8eb2171e5a38a9d6a4ad6f1d4859
th62 <haberland@webtaktik.de>
2023-01-27 22:12:27 +0100

    CreatePdf.java -> variable für die Personenliste.pdf umbenannt

--- src/main/java/de/unibremen/akademie/kursverwaltung/application/CreatePdf.java
+++ src/main/java/de/unibremen/akademie/kursverwaltung/application/CreatePdf.java
@@ -3,6 +3,7 @@ package de.unibremen.akademie.kursverwaltung.application;
 import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.draw.DottedLine;
 import com.itextpdf.kernel.pdf.canvas.draw.SolidLine;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.LineSeparator;
@@ -20,22 +21,23 @@ public class CreatePdf {
     public void createPdf(String dest) throws IOException {
         PdfWriter writer = new PdfWriter(dest);
         PdfDocument pdf = new PdfDocument(writer);
-        Document document = new Document(pdf, PageSize.A4);
 
         // Layout-Elemente
         SolidLine line = new SolidLine(1f);
         LineSeparator separator = new LineSeparator(line);
 
-        document.add(new Paragraph("Liste aller gespeicherten Personen"));
-        document.add(separator);
-        document.add(new Paragraph(""));
-
+        //Dokument für die Personenliste erstellen
+        Document personenListePdf = new Document(pdf, PageSize.A4);
+        personenListePdf.add(new Paragraph("Liste aller gespeicherten Personen"));
+        personenListePdf.add(separator);
+        personenListePdf.add(new Paragraph(""));
         for (Object obj : kvModel.getPersonen().getPersonenListe()) {
             Person person = (Person) obj;
-            document.add(new Paragraph(person.personToPDF()));
-            document.add(separator);
+            personenListePdf.add(new Paragraph(person.personToPDF()));
+            personenListePdf.add(separator);
         }
+        personenListePdf.add(separator);
+        personenListePdf.close();
 
-        document.close();
     }
 }
