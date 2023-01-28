366fea943132e41ee46a35725df6865324f35dc7
th62 <haberland@webtaktik.de>
2023-01-28 12:25:49 +0100

    Erneute pom.xml Korrektur (iText 7.2.5) und Metadaten zum PDF hinzugefügt

--- src/main/java/de/unibremen/akademie/kursverwaltung/application/CreatePdf.java
+++ src/main/java/de/unibremen/akademie/kursverwaltung/application/CreatePdf.java
@@ -1,39 +1,36 @@
 package de.unibremen.akademie.kursverwaltung.application;
 
-import com.itextpdf.kernel.geom.PageSize;
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.kernel.pdf.canvas.draw.DottedLine;
-import com.itextpdf.kernel.pdf.canvas.draw.SolidLine;
+import com.itextpdf.kernel.pdf.*;
+import com.itextpdf.kernel.pdf.annot.PdfAnnotation;
 import com.itextpdf.layout.Document;
+import de.unibremen.akademie.kursverwaltung.domain.Person;
+
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.canvas.draw.SolidLine;
 import com.itextpdf.layout.element.LineSeparator;
 import com.itextpdf.layout.element.Paragraph;
-import de.unibremen.akademie.kursverwaltung.domain.Person;
-import javafx.scene.paint.Color;
 
-import java.io.File;
 import java.io.IOException;
 
-import static de.unibremen.akademie.kursverwaltung.domain.AnwendungsModel.aktuellePerson;
 import static de.unibremen.akademie.kursverwaltung.domain.AnwendungsModel.kvModel;
 
 public class CreatePdf {
 
     // Zu erzeugende Dateien
-    String DEST = "src/main/resources/de/unibremen/akademie/kursverwaltung/pdf/Personenliste.pdf";
-    File file = new File(DEST);
-
+    String PERSONENLISTEPDF = "src/main/resources/de/unibremen/akademie/kursverwaltung/pdf/Personenliste.pdf";
 
     // Layout-Elemente
     public SolidLine line = new SolidLine(1f);
     public LineSeparator separator = new LineSeparator(line);
 
     public void createPersonenListePdf() throws IOException {
-        //file.getParentFile().mkdirs();
-        PdfWriter writer = new PdfWriter(DEST);
-        PdfDocument pdf = new PdfDocument(writer);
-
-        //Dokument für die Personenliste erstellen
+        PdfDocument pdf = new PdfDocument(
+                new PdfWriter(PERSONENLISTEPDF,
+                        new WriterProperties()
+                                .addXmpMetadata()
+                                .setPdfVersion(PdfVersion.PDF_1_6)));
+        PdfDocumentInfo info = pdf.getDocumentInfo();
+        addMetaData(info, "Personenliste"); //Metadaten hinzufuegen
         Document personenListePdf = new Document(pdf, PageSize.A4);
         personenListePdf.add(new Paragraph("Liste aller gespeicherten Personen"));
         personenListePdf.add(separator);
@@ -43,7 +40,6 @@ public class CreatePdf {
             personenListePdf.add(new Paragraph(personToPDF(person)));
             personenListePdf.add(separator);
         }
-        personenListePdf.add(separator);
         personenListePdf.close();
     }
 
@@ -57,5 +53,13 @@ public class CreatePdf {
                 person.getStrasse() + "\r" +
                 person.getEmail() + " " +
                 person.getTelefon();
+    }
+
+    private static void addMetaData(PdfDocumentInfo info, String subject) {
+            info.setTitle("Kursverwaltung");
+            info.setAuthor("SWE 2022/2023");
+            info.setSubject(subject);
+            info.setKeywords("M. Dbour | F. Ennouali | A. Förster | T. Haberland | Y. Hanna | A. Karaköse | R. Lukas | M. Pawel | O. Thiel | T. Tomy");
+            info.setCreator("SWE-M07 - DevelopmentGroup");
     }
 }
