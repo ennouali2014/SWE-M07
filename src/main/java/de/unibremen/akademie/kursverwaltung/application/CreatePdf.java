4bf9268336e0874bedaf8b90f59c53972f65d8bb
th62 <haberland@webtaktik.de>
2023-01-28 14:59:22 +0100

    Erstellungsdatum zu Überschrift hinzugefügt, Personenliste.pdf

--- src/main/java/de/unibremen/akademie/kursverwaltung/application/CreatePdf.java
+++ src/main/java/de/unibremen/akademie/kursverwaltung/application/CreatePdf.java
@@ -14,24 +14,29 @@ import com.itextpdf.layout.element.LineSeparator;
 import com.itextpdf.layout.element.Paragraph;
 
 import java.io.IOException;
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class CreatePdf {
 
     // Zu erzeugende Dateien
     String PERSONENLISTEPDF = "src/main/resources/de/unibremen/akademie/kursverwaltung/pdf/Personenliste.pdf";
+    String KURSELISTEPDF = "src/main/resources/de/unibremen/akademie/kursverwaltung/pdf/Kurseliste.pdf";
+    String ANWESENHEITSLISTEPDF = "src/main/resources/de/unibremen/akademie/kursverwaltung/pdf/Anwesenheitsliste.pdf";
 
     // Layout-Elemente
-
-
     public SolidLine line = new SolidLine(1f);
     public LineSeparator separator = new LineSeparator(line);
 
+    // aktuelles Datum
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+    String aktuellesDatum = dateFormat.format(new Date());
+
     public void createPersonenListePdf() throws IOException {
-        String headline = "Liste aller gespeicherten Personen";
+        String headline = "Liste aller gespeicherten Personen (Stand: " + aktuellesDatum +")";
         String metaSubject = "Personenliste";
-        String tabsAbstand = "\t\t\t\t\t\t\t\t\t\t";
+        String tabsAbstand = "\t\t\t";
         int counterPersonen = 0;
         int anzahlPersonenJeSeite = 10;
         int seiteAktuell = 1;
@@ -45,8 +50,9 @@ public class CreatePdf {
         PdfDocumentInfo info = pdf.getDocumentInfo();
         addMetaData(info, metaSubject); //Metadaten hinzufuegen
         Document personenListePdf = new Document(pdf, PageSize.A4);
+
         if (seitenGesamt > 0) {
-            personenListePdf.add(new Paragraph(headline + tabsAbstand + seiteAktuell + "/" + seitenGesamt).setFontSize(18));
+            personenListePdf.add(new Paragraph(headline + tabsAbstand + seiteAktuell + "/" + seitenGesamt).setFontSize(16));
             personenListePdf.add(separator);
             personenListePdf.add(new Paragraph("\n\n"));
             for (Object obj : kvModel.getPersonen().getPersonenListe()) {
@@ -58,7 +64,7 @@ public class CreatePdf {
                 } else {
                     personenListePdf.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
                     seiteAktuell++;
-                    personenListePdf.add(new Paragraph(headline + tabsAbstand + seiteAktuell + "/" + seitenGesamt).setFontSize(18));
+                    personenListePdf.add(new Paragraph(headline + tabsAbstand + seiteAktuell + "/" + seitenGesamt).setFontSize(16));
                     personenListePdf.add(separator);
                     personenListePdf.add(new Paragraph("\n\n"));
                     personenListePdf.add(new Paragraph(personToPDF(person)));
@@ -68,8 +74,9 @@ public class CreatePdf {
             }
         } else {
             personenListePdf.add(new Paragraph(headline).setFontSize(18));
-            personenListePdf.add(new Paragraph("\nKeine sind keine Personen in der Kursverwaltung angelegt!").setFontSize(24));
+            personenListePdf.add(new Paragraph("\nKeine sind keine Personen in der Kursverwaltung angelegt!").setFontSize(18));
         }
+
         personenListePdf.close();
     }
 
@@ -87,9 +94,9 @@ public class CreatePdf {
 
     private static void addMetaData(PdfDocumentInfo info, String subject) {
             info.setTitle("Kursverwaltung");
-            info.setAuthor("SWE 2022/2023");
+            info.setAuthor("SWE-M07 - DevelopmentGroup");
             info.setSubject(subject);
             info.setKeywords("M. Dbour | F. Ennouali | A. Förster | T. Haberland | Y. Hanna | A. Karaköse | R. Lukas | M. Pawel | O. Thiel | T. Tomy");
-            info.setCreator("SWE-M07 - DevelopmentGroup");
+            info.setCreator("SWE 2022/2023 - Kursverwaltung v1.0");
     }
 }
