59536858ddb2c17a8151b3b809ee161e250d5b93
Alex <afoerster@me.com>
2023-02-02 10:20:25 +0100

    loadsavetest korrigiert ohne feste strings

--- src/Test/de/unibremen/akademie/kursverwaltung/domain/KvModelTest.java
+++ src/Test/de/unibremen/akademie/kursverwaltung/domain/KvModelTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 
-import static de.unibremen.akademie.kursverwaltung.domain.AnwendungsModel.kvModel;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class KvModelTest {
@@ -21,15 +20,17 @@ class KvModelTest {
 
     @Test
     void saveLoadtest() {
-        testModel.getPersonen().addNewPerson("Herr", "Dr. rer. nat.", "Alexander", "Förster", "Feldweg 17", "28195", "Bremen", "axf@uni-bremen.de", "+49 162 175 978 23");
-        testModel.getKurse().addNewKurs("php", 12, 3, new Date(1898553600000L), 1, 10, 150, 19, "php backend", "Aktiv");
-        testModel.getPkListe().addPersonInKursAlsTeilnehmer(kvModel.getPersonen().getPersonVonPersonenListe(0), kvModel.getKurse().getKursVonKursListe(0));
+        Person person = testModel.getPersonen().addNewPerson("Herr", "Dr. rer. nat.", "Alexander", "Förster", "Feldweg 17", "28195", "Bremen", "axf@uni-bremen.de", "+49 162 175 978 23");
+        Kurs kurs = testModel.getKurse().addNewKurs("php", 12, 3, new Date(1898553600000L), 1, 10, 150, 19, "php backend", "Aktiv");
+        testModel.getPkListe().addPersonInKursAlsTeilnehmer(testModel.getPersonen().getPersonVonPersonenListe(0), testModel.getKurse().getKursVonKursListe(0));
         testModel.save(VERWALTUNGSDATEITEST);
         testModel.getKurse().getKursListe().clear();
         testModel.getPersonen().getPersonenListe().clear();
         testModel.load(VERWALTUNGSDATEITEST);
-        assertEquals(kvModel.getPersonen().getPersonenListe().toString(), "[Person{anrede='StringProperty [value: Herr]'titel='StringProperty [value: Dr. rer. nat.]'vorname='StringProperty [value: Alexander]', nachname='StringProperty [value: Förster]', strasse='StringProperty [value: Feldweg 17]', plz='StringProperty [value: 28195]', ort='StringProperty [value: Bremen]', email='StringProperty [value: axf@uni-bremen.de]', telefon='StringProperty [value: +49 162 175 978 23]'}]");
-        assertEquals(kvModel.getKurse().getKursListe().toString(), "[Kurs{name=php, anzahlTage=12, zyklus=3, startDatum=1898553600000, minTnZahl=1, maxTnZahl=10, gebuehrBrutto=150.0, mwstProzent=19.0, kursBeschreibung=php backend, endeDatum=1900972800000, status=Aktiv, gebuehrNetto=126.05, mwstEuro=23,95, freiePlaetze=10, aktuelleTnZahl=0}]");
+        assertEquals(1, testModel.getPersonen().getPersonenListe().size());
+        assertEquals(1, testModel.getKurse().getKursListe().size());
+        assertEquals(testModel.getPersonen().getPersonenListe().get(0).toString(), person.toString());
+        assertEquals(testModel.getKurse().getKursListe().get(0).toString(), kurs.toString());
     }
 
 }
