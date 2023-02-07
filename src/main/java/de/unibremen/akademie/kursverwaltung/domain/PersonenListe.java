5e03e89d035bbf52333efe16734e821285d63e17
Yolla <yhanna@uni-bremen.de>
2023-02-07 11:43:21 +0100

    PersonenListe Klasse JavaDoc 2

--- src/main/java/de/unibremen/akademie/kursverwaltung/domain/PersonenListe.java
+++ src/main/java/de/unibremen/akademie/kursverwaltung/domain/PersonenListe.java
@@ -18,6 +18,19 @@ public class PersonenListe {
         return personenliste.get(index);
     }
 
+    /**
+     * @param anrede
+     * @param titel
+     * @param vorname
+     * @param nachname
+     * @param strasse
+     * @param plz
+     * @param ort
+     * @param email
+     * @param telefon
+     * @return wir verwenden diese Methode, um eine neue Person in die Observable Liste der Personen aufzusetzen.
+     */
+
     public Person addNewPerson(String anrede, String titel, String vorname, String nachname, String strasse, String plz, String ort, String email, String telefon) {
         Person person = new Person(anrede, titel, vorname, nachname, strasse, plz, ort, email, telefon);
         addPersonenZuListe(person);
